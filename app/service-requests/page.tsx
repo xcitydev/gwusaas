440c474af7cc1c6860fbb8ea61127bfe85bc13fd
G-veyron <64647601+xcitydev@users.noreply.github.com>
2025-08-12 19:30:10 +0100

    openai

---
+++ app/service-requests/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-center">Service Requests</h1>
+    </div>
+  );
+};
+
+export default page;
